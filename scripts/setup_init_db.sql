d114a7f3cedf254742c95d0b819273ddda28c341
Kyle Samson <ksamson8505@gmail.com>
2015-10-21 17:14:57 -0400

    added directions to script

--- scripts/setup_init_db.sql
+++ scripts/setup_init_db.sql
@@ -1,3 +1,5 @@
+-- Run with this command from the same directory as this script
+--  sudo su postgres -c "psql -f setup_init_db.sql"
 CREATE DATABASE convention;
 CREATE ROLE application WITH LOGIN PASSWORD 'hello';
 ALTER DATABASE convention OWNER TO application;
