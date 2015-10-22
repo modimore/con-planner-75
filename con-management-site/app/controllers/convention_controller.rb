fb7a8736e3d8f4e64759b85159a9c5f9f2139620
modimore <sirmodimore@gmail.com>
2015-10-22 15:39:02 -0400

    add new pages to site

---
+++ con-management-site/app/controllers/convention_controller.rb
@@ -0,0 +1,16 @@
+class ConventionController < ApplicationController
+  def index
+  end
+
+  def events
+  end
+
+  def schedule
+  end
+
+  def documents
+  end
+
+  def details
+  end
+end
---
+++ con-management-site/app/helpers/convention_helper.rb
@@ -0,0 +1,2 @@
+module ConventionHelper
+end
